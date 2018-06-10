4784fab106ca00e919c6e5316b2b64426cee93d5
Dylan Schroeder <schrodac@gmail.com>
2018-06-10 18:53:23 -0500

    finished problem 4

--- app.js
+++ app.js
@@ -94,12 +94,18 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumArray(sumArr) {
-
+  var sumOfValues = 0;
+  for(var i = 0; i < 3; i++){
+    sumOfValues = sum(sumOfValues,sumArr[i])[0];
+  }
+  var array = [sumOfValues, sumArr[0] + ',' + sumArr[1] + ',' + sumArr[2] + ' was passed in as an array of numbers, and ' + sumOfValues + ' is their sum.'];
+  console.log(array);
+  return array;
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray([2, 3, 4]);
+testSumArray([2, 3, 4]);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
